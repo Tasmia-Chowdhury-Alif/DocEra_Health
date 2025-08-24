79557dbe8381a9002dd0d0c6174bfcb4940d119f
Tasmia-Chowdhury-Alif <tasmiachowdhuryalif222@gmail.com>
2025-08-24 19:55:53 +0600

    removed old unnecessery endpoints for Registration, Login, Logout

--- DocEra_Health_api/patient/views.py
+++ DocEra_Health_api/patient/views.py
@@ -22,69 +22,69 @@ class PatientViewset(viewsets.ModelViewSet):
     serializer_class = serializers.PatientSerializer
 
 
-class RegistrationApiView(APIView):
-    serializer_class = serializers.RegistrationSerializer
+# class RegistrationApiView(APIView):
+#     serializer_class = serializers.RegistrationSerializer
 
 
-    def post(self, request):
-        serializer = self.serializer_class(data= request.data)
+#     def post(self, request):
+#         serializer = self.serializer_class(data= request.data)
 
-        if serializer.is_valid():
-            user = serializer.save()
-            token = default_token_generator.make_token(user)
-            print("token : ", token)
-            uid = urlsafe_base64_encode(force_bytes(user.pk))
-            print("uid : ", uid)
-            confirm_link = f"http://127.0.0.1:8000/patient/activate/{uid}/{token}"
-            email_subject = "Smart Care Account Varification"
-            email_body = render_to_string('patient/confirm_email.html', {"confirm_link" : confirm_link})
+#         if serializer.is_valid():
+#             user = serializer.save()
+#             token = default_token_generator.make_token(user)
+#             print("token : ", token)
+#             uid = urlsafe_base64_encode(force_bytes(user.pk))
+#             print("uid : ", uid)
+#             confirm_link = f"http://127.0.0.1:8000/patient/activate/{uid}/{token}"
+#             email_subject = "Smart Care Account Varification"
+#             email_body = render_to_string('patient/confirm_email.html', {"confirm_link" : confirm_link})
 
-            email = EmailMultiAlternatives(email_subject, '', to=[user.email])
-            email.attach_alternative(email_body, "text/html")
-            email.send()
+#             email = EmailMultiAlternatives(email_subject, '', to=[user.email])
+#             email.attach_alternative(email_body, "text/html")
+#             email.send()
 
-            return Response("Check your Email for confirmation")
-        return Response(serializer.errors)
+#             return Response("Check your Email for confirmation")
+#         return Response(serializer.errors)
     
 
-def activate(request, uid64, token):
-    try:
-        uid = urlsafe_base64_decode(uid64).decode()
-        user = User._default_manager.get(pk= uid)
-    except(User.DoesNotExist):
-        user = None
+# def activate(request, uid64, token):
+#     try:
+#         uid = urlsafe_base64_decode(uid64).decode()
+#         user = User._default_manager.get(pk= uid)
+#     except(User.DoesNotExist):
+#         user = None
 
-    if user is not None and default_token_generator.check_token(user, token):
-        user.is_active = True 
-        user.save()
-        return redirect('login')
-    else :
-        return redirect('register')
+#     if user is not None and default_token_generator.check_token(user, token):
+#         user.is_active = True 
+#         user.save()
+#         return redirect('login')
+#     else :
+#         return redirect('register')
 
 
-class LoginApiView(APIView):
-    def post(self, request):
-        serializer = serializers.UserLoginSerializer(data= self.request.data)
-        if serializer.is_valid():
-            username = serializer.validated_data['username']
-            password = serializer.validated_data['password']
+# class LoginApiView(APIView):
+#     def post(self, request):
+#         serializer = serializers.UserLoginSerializer(data= self.request.data)
+#         if serializer.is_valid():
+#             username = serializer.validated_data['username']
+#             password = serializer.validated_data['password']
 
-            user = authenticate(username= username, password= password)
+#             user = authenticate(username= username, password= password)
 
-            if user :
-                token, _ = Token.objects.get_or_create(user= user)
-                print(token)
-                print(_)
-                login(request, user= user)
-                return Response({'token' : token.key, 'user_id' : user.id})
-            else :
-                return Response({'error' : "Invalid Credential"})
-        return Response(serializer.errors)
+#             if user :
+#                 token, _ = Token.objects.get_or_create(user= user)
+#                 print(token)
+#                 print(_)
+#                 login(request, user= user)
+#                 return Response({'token' : token.key, 'user_id' : user.id})
+#             else :
+#                 return Response({'error' : "Invalid Credential"})
+#         return Response(serializer.errors)
                 
 
-class LogoutApiView(APIView):
-    permission_classes = [IsAuthenticated]
-    def get(self, request):
-        request.user.auth_token.delete()
-        logout(request)
-        return redirect('login')
+# class LogoutApiView(APIView):
+#     permission_classes = [IsAuthenticated]
+#     def get(self, request):
+#         request.user.auth_token.delete()
+#         logout(request)
+#         return redirect('login')
