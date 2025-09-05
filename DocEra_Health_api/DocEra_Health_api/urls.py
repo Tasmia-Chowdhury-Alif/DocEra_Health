dd5afe7f1add7e2540e655cd5554d1b44b8e9fa2
Tasmia-Chowdhury-Alif <tasmiachowdhuryalif222@gmail.com>
2025-09-05 23:59:38 +0600

    removed patient registration endpoint

--- DocEra_Health_api/DocEra_Health_api/urls.py
+++ DocEra_Health_api/DocEra_Health_api/urls.py
@@ -3,20 +3,13 @@ from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
 
-from patient.views import PatientRegistrationView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
     re_path(r'^auth/', include('djoser.urls')),  # /auth/users/, /auth/users/me/
     re_path(r'^auth/', include('djoser.urls.jwt')),  # /auth/jwt/create/, etc.
-
-    path('auth/patients/register/', PatientRegistrationView.as_view({'post': 'create'}), name='patient-register'),
 
     path('contact_us/', include('contact_us.urls')),
     path('service/', include('service.urls')),
